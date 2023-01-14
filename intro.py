2edf1237c83a6cb4c2d5ba54c20554b4846bb950
RAKESH40-lgtm <ram19042k@protonmail.com>
2023-01-14 15:52:27 +0530

    intro of python

---
+++ intro.py
@@ -0,0 +1,31 @@
+"""
+Programming language:Which is invented for humans as computer understand only 0 and 1 ,to make interactive to computer we use PL which can written in two type of language either low-level (equivalent language) or high-level(Python,javaScript,java etc) and this change to machine level language an interpreter or compiler is used . Note:all programming language do and give result.
+Interpreter (translator):reads line by line instruction and converts to machine level code and excutes.
+Compiler (translators):reads entire file at once and then converts to machine level code and executes.
+Python is an interpreter language(which executes line-by-line) interpreters{(cpython(written in c popular one),jyhton(written in java) ,pypy(written in python itself))} by this it read the line by line and execute it. when we write the python code through interpreter it first convert source code to bytecode(which is machine-level code) and through virtual machine it shows us in the output screen.python file extension is (.py)  and which is slower than c,java.
+But good at
+1)easy to learn and productivity 
+2)less line .
+3)visualizing,processing ,webservice and many more.
+
+ways to execute python code:
+(replits )online 
+1)Terminal:cmd prompt using python
+2)code Editors:like visualStudioCode and sublimetext editor
+3)IDEs(integrate development environment):like pycharm and spyder
+4)Notebooks(for ML):like jupyter
+version is about changing or enhancing the feature of programming language 
+Python2 v/s Python3:
+Guido van rossum developed python
+python2:from starting of invention till 2008 python2 there later it evolve to python3 and it's legacy,has less community and security than python3.
+python3 has changes of minor like print and  and this is popular one 
+And all community moving to python3
+Major diff between python2 and python3 are:
+printing,division operator,unicode,xrange,errorHandling and future module.]
+based on pros and cons of programming language to develop certain we choose programming language
+"""
+#to print the name or anything
+print("Rakesh")
+#input() capture the information from user  with message inside it and can store in variable and print that with associated name
+name=input("Enter you're name")
+print("hello ",name)
